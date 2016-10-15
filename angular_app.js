ccf8f2b4ab38d056bea562eb9e8529d7489c1049
brendanAlbert <alber123@mail.chapman.edu>
2016-10-14 21:11:47 -0700

    Update angular_app.js

--- angular_app.js
+++ angular_app.js
@@ -2,16 +2,25 @@
 var app = angular.module("myShoppingList", []);
 app.controller("myCtrl", function($scope) {
   $scope.products = ["Milk", "Bread", "Cheese"];
-  $scope.addItem = function () {
+  
+  $scope.addItem = function ()
+  {
     $scope.errortext = "";
+
     if (!$scope.addMe) {return;}
-    if ($scope.products.indexOf($scope.addMe) == -1) {
-      $scope.products.push($scope.addMe);
-    } else {
-      $scope.errortext = "The item is already in your shopping list.";
+    
+    if ($scope.products.indexOf($scope.addMe) == -1)
+    {
+       $scope.products.push($scope.addMe);
+    }
+    else
+    {
+       $scope.errortext = "The item is already in your shopping list.";
     }
   }
-  $scope.removeItem = function (x) {
+  
+  $scope.removeItem = function (x)
+  {
     $scope.errortext = "";
     $scope.products.splice(x, 1);
   }
